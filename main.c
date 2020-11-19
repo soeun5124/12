142b1df0dd71d4cef999c518220c72b7c761ae90
soeun5124 <71041198+soeun5124@users.noreply.github.com>
2020-11-19 20:43:53 +0900

    submission01

--- main.c
+++ main.c
@@ -1,39 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	FILE *fp;
-	char str[100];
-	char path[100];
-	char lookup[100];
+	char code1 = 'A';
+	char code2 = 65;
 	
-	printf("input file path:");
-	scanf("%s", path);
-	printf("input search word:");
-	scanf("%s",lookup);
-	
-	fp = fopen(path, "r");
-	if (fp == NULL)
-	{
-		printf("invalid path! (%s)\n", path);
-		return -1;
-	}
-	
-	while ( fgets(str, 100, fp) != NULL) {
-		if (strncmp(str, lookup, strlen(lookup) ) == 0)
-		{
-			printf("Search succeed!\n");
-			fclose(fp);
-			return 0;
-		} 
-	}
-	
-	printf("Search failed!\n");
-	fclose(fp);
+	printf("code1 = %c (%d)\n", code1, code1);
+	printf("code2 = %c (%d)\n", code2, code2);
 	
 	return 0;
 }
